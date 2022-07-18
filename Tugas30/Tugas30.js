92d729836708d1d04c1f9fab9ae54b28fda24488
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 20:29:05 +0700

    tugas 30 js
    untuk niomic

---
+++ Tugas30/Tugas30.js
@@ -0,0 +1,6 @@
+function panggilRegexp(value) {
+   var ambilData = value.match(/["s","e"]/g)
+   console.log(ambilData)
+}
+
+panggilRegexp("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
